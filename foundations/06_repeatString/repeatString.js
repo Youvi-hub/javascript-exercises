857fe78b517749047f2dc29a63bea6eb15e2d811
Youvraj <youvi572@gmail.com>
2026-03-13 19:01:05 +0200

    Repeat "hello" 10 times
    For this task I left my finalSting empty, then did the loop and finally returned the finalString. After that I stored my value in the result printed it.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,11 +1,15 @@
 const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
+  let finalString = "";
+
+  for (let i = 0; i< times; i++) {
+    finalString += word;
   }
-  return string;
+
+  return finalString;
 };
+
+let result = repeatString("hello", 10);
+console.log (result);
 
 
 // Do not edit below this line
